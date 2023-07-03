1872e657d7ca577076e22b3a95e394356b7b04a4
Algorithme123 <kossi.gogo@ipnetinstitute.com>
2023-07-03 23:26:58 +0000

    upload file OK

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { HttpClientModule,HttpErrorResponse,HttpResponse,HttpEventType  } from '@angular/common/http';
+import {HttpClient} from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'uploadFile';
+
+  fileToUpload: File | null = null;
+  fileUploadResponse: any;
+
+  constructor(private http: HttpClient) { }
+
+  onFileSelected(event: any) {
+    this.fileToUpload = event.target.files[0];
+  }
+
+  onUpload() {
+    if (this.fileToUpload) {
+      const formData = new FormData();
+      formData.append('image', this.fileToUpload, this.fileToUpload.name);
+
+      this.http.post<any>('http://localhost:8080/file/upload', formData)
+        .subscribe(
+          (response: HttpResponse<any>) => {
+            this.fileUploadResponse = response.body;
+          },
+          (error: HttpErrorResponse) => {
+            console.error(error);
+            this.fileUploadResponse = { fileName: null, message: 'Error occurred during file upload' };
+          }
+        );
+    }
+  }
+
 }
